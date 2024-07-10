f64881e9c5c6333791f843a0d8cc9dfe49f9b0fb
Leonardo Hanon <hanon.leonardo@gmail.com>
2024-07-10 11:50:24 -0400

    add clear and complete icons

--- src/CreateLogger.tsx
+++ src/CreateLogger.tsx
@@ -1,6 +1,8 @@
+import { Box, Button, IconButton, TextField } from "@mui/material";
 import { FormEvent, useEffect, useRef, useState } from "react";
 
-import { Button } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import ClearIcon from "@mui/icons-material/Clear";
 
 type CreateLoggerProps = {
   onSubmit: (name: string) => void;
@@ -28,23 +30,35 @@ export function CreateLogger({ onSubmit }: CreateLoggerProps) {
   }, [editMode]);
 
   return (
-    <>
+    <Box sx={{ height: 72, display: "flex", alignItems: "stretch" }}>
       {!editMode && (
-        <Button sx={{ height: 72, fontSize: "medium" }} onClick={handleClick}>
+        <Button sx={{ fontSize: "medium" }} fullWidth onClick={handleClick}>
           + Create New
         </Button>
       )}
       {editMode && (
-        <form onSubmit={handleSubmit}>
-          <input
-            className="createButton"
-            ref={inputRef}
-            type="text"
+        <form
+          onSubmit={handleSubmit}
+          style={{ width: "100%", display: "flex", alignItems: "center" }}
+        >
+          <TextField
+            inputRef={inputRef}
             value={name}
             onChange={(e) => setName(e.target.value)}
+            fullWidth
           />
+          <IconButton
+            onClick={() => {
+              setEditMode(false);
+            }}
+          >
+            <ClearIcon fontSize="large" />
+          </IconButton>
+          <IconButton type="submit">
+            <CheckIcon fontSize="large" />
+          </IconButton>
         </form>
       )}
-    </>
+    </Box>
   );
 }
