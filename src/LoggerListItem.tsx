e197d9943a553cb73064ec56cf145160bbbdabb4
Leonardo Hanon <hanon.leonardo@gmail.com>
2024-07-16 07:00:30 -0400

    add up and down buttons to logg edit mode

--- src/LoggerListItem.tsx
+++ src/LoggerListItem.tsx
@@ -1,8 +1,19 @@
-import { Box, IconButton, Paper, Tooltip, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Paper,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import { DeleteButton } from "./DeleteButton";
 import ElectricBoltIcon from "@mui/icons-material/ElectricBolt";
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import { Padding } from "@mui/icons-material";
 import { useLoggerListContext } from "./LoggerListContext";
 import { useNavigate } from "react-router-dom";
 
@@ -33,16 +44,28 @@ export function LoggerListItem({
         }}
       >
         <Box sx={{ display: "flex", alignItems: "center" }}>
-          {!editMode && (
-            <Tooltip title="View">
-              <IconButton
-                onClick={() => navigate(`logs/${logId}`, { replace: true })}
-              >
-                <MenuOpenIcon fontSize="large" />
-              </IconButton>
-            </Tooltip>
-          )}
-          <Typography sx={{ marginLeft: 1 }}>{value}</Typography>
+          <Stack direction={"row"} spacing={2} alignItems={"center"}>
+            {!editMode && (
+              <Tooltip title="View">
+                <IconButton
+                  onClick={() => navigate(`logs/${logId}`, { replace: true })}
+                >
+                  <MenuOpenIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+            )}
+            {editMode && (
+              <Stack direction="row" spacing={1}>
+                <Button variant="outlined" sx={{ padding: "5px", minWidth: 0 }}>
+                  <ArrowDropUpIcon fontSize="large" />
+                </Button>
+                <Button variant="outlined" sx={{ padding: "5px", minWidth: 0 }}>
+                  <ArrowDropDownIcon fontSize="large" />
+                </Button>
+              </Stack>
+            )}
+            <Typography sx={{ marginLeft: 1 }}>{value}</Typography>
+          </Stack>
         </Box>
         <Box>
           {/* {!editMode && (
